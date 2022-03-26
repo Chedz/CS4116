51c0856a1231de68535626416f1273cb0a7043d9
Danielz110 <64483972+Danielz110@users.noreply.github.com>
2022-03-26 04:30:01 +0000

    Update search.php

--- search.php
+++ search.php
@@ -1,50 +1,31 @@
+<!DOCTYPE html>
+ <html>
+ 
+ <head>
+   <title>Search</title>
+   <link rel="stylesheet" href="css/style.css" type="text/css">
+ </head>
+
+<body>
+<h1>Search</h1>
+				<form action="search.php" method="POST">
+					<input type="text" name="search" placeholder="Search for members..."/>
+					<button type="submit" name="Search">Search</button>
+	
+				</form>
+
 <?php
 
-	require_once 'includes/dbh.inc.php';
-	$conn = createConnection("sql100.epizy.com", "epiz_31242413", "WbIh2OaPZju", "epiz_31242413_project_database");
-$output = '';
-if(isset($_POST['search']))  {
-		$searchq = $_POST['search'];
-		$searchq = preg_replace("#[^0-9a-z]#i","",$searchq );
-		
-		$query = mysql_query("SELECT * FROM users WHERE firstname LIKE '%$searchq%' OR lastname LIKE '%$searchq%'") or die("could not search");
-		$count = mysql_num_rows($query);
-		if($count == 0){
-			$output = 'There was no search results!';
-		}else{
-			while($row = mysqp_fetch_array($query))  {
-			$fname = $row['firstname'];
-			$lname = $row['lname'];
-			$id = $row['id'];
-			
-			$output .= '<div>'.$fname.' '.$lname.'</div>';
-		}
-		}
+if (isset($_POST["search"])) {
+  require "searchPage.php";
+
+
+  if (count($results) > 0) { 
+      foreach ($results as $r) {
+        printf("<div>%s - %s</div>", $r["Firstname"], $r["Surname"]);
+  }} else { echo "No results found"; }
 }
 ?>
 
-<!Doctype html>
-<html>
-<head>
-
-<title>Search</title>
-<!--<meta http-equiv = "Content-Type" content = "text/html; url = http://14-cs4116.infinityfreeapp.com/search.php" />-->
-<meta http-equiv = "Content-Type" content = "text/html; charset=utf-8" />
-
-</head>
-
-
-
-<body>
-
-<form action="Search.php" method="post">
-	<input type="text" name="search" placeholder="Search for members..."/>
-	<input type="submit" value="Search" />
-
-</form>
-
-<?php print("$output");?>
 </body>
-
-
 </html>
