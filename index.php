55cac0c2b1201624c527123b51d3a402fd2711fd
Eoin Chedzey <eoinisnow@gmail.com>
2022-03-29 19:09:36 +0100

    started styling register page and added logo

--- index.php
+++ index.php
@@ -1,76 +1,104 @@
-<?php
-  require_once 'includes/dbh.inc.php';
-  $mysqli = createConnection("sql100.epizy.com", "epiz_31242413", "WbIh2OaPZju", "epiz_31242413_project_database");
- ?>
-
- <!DOCTYPE html>
- <html>
- <head>
-   <title>Home</title>
- </head>
- <body>
-
-   <?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+ <title>Document</title>
+</head>
+<body>
+ <?php
    session_start();
    if(!empty($_SESSION['loggedin'])){ header("location: home.php");}  //check if user already logged in
+   // $email= $_POST['email'];
+   // $name= $_POST['user'];
+   // $password = $_POST['password'];
 
-   // echo "_POST array";
-   // pre_r($_POST);
-   // echo "_GET array";
-   // pre_r($_GET);
-   // echo "_REQUEST array";
-   // pre_r($_REQUEST);
-
-   if (isset($_POST['submit'])) { //the POST form has been submitted
-     echo "First name:".$_POST['firstname'].'<br />';
-     echo "Surname:".$_POST['surname'].'<br />';
+   if(isset($_POST['register'])) {
+       echo "First name:".$_POST['firstname'].'<br />';
+       echo "Surname:".$_POST['surname'].'<br />';
+       echo "Password:".$_POST['password'].'<br />';
+       echo "Email:".$_POST['email'].'<br />';
+     // Check if name has been entered
+     if(empty($_POST['Firtsname'])) {
+       $errName= 'Please enter your first name';
+     }
+     if(empty($_POST['Surname'])) {
+       $errName= 'Please enter your surname';
+     }
+     // Check if email has been entered and is valid
+     else if(empty($_POST['email'])) {
+       $errEmail = 'Please enter a valid email address';
+     }
+     // check if a password has been entered and if it is a valid password
+     else if(empty($_POST['password']) || (preg_match("/^.*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$/", $_POST["password"]) === 0)) {
+       $errPass = '<p class="errText">Password must be at least 8 characters and must contain at least one lower case letter, one upper case letter and one digit</p>';
+     } else {
+       echo "The form has been submitted";
+     }
    }
 
-   //$mysqli->close();
-   closeConnection($mysqli);
-    ?>
+    // if (isset($_POST['submit'])) { //the POST form has been submitted
+    //   echo "First name:".$_POST['firstname'].'<br />';
+    //   echo "Surname:".$_POST['surname'].'<br />';
+    // }
 
-    <h1> Registration </h1>
+    if(array_key_exists('buttonSignIn', $_POST)) { //go to sign in
+        header("location: login.php");
+    }
 
-   <form action = "register.php" method="POST">
-
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="text" class="form-control" id="email" name="email" required/>
+ ?>
+ <div class="container">
+   <div class="row" style="min-width:800">
+      <div class="col-sm-1"><img src="images/uniConnectLogo.png" alt="Logo" ></div>
+      <!-- <div class="col-sm-11 text-end text-uppercase">
+         <h1>Header1</H1><H3>This is Header3</h3></div> -->
+     </div>
+   <form role="form" method="post" action = "register.php" method="POST">
+     <div class="form-group row">
+       <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+       <div class="col-sm-10">
+         <input type="email" class="form-control" id="inputEmail" name="email" placeholder="Email" required>
+         <?php echo $errEmail; ?>
+       </div>
+     </div>
+     <div class="form-group row">
+      <label for="inputFirst" class="col-sm-2 col-form-label">First Name</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="inputFirst" name="firstname" placeholder="First Name" required>
+        <?php echo $errName; ?>
+      </div>
     </div>
-    <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" name="password" required/>
+    <div class="form-group row">
+      <label for="inputSurname" class="col-sm-2 col-form-label">Surname</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="inputSurname" name="surname" placeholder="Surname" required>
+        <?php echo $errName; ?>
     </div>
-    <div class="form-group">
-        <label for="Firstname">First Name</label>
-        <input type="text" class="form-control" id="Firstname" name="Firstname" required/>
     </div>
-    <div class="form-group">
-        <label for="Surname">Last Name</label>
-        <input type="text" class="form-control" id="Surname" name="Surname" required/>
-    </div>
-    <input type="submit" class="btn btn-primary"/>
-    </form>
+     <div class="form-group row">
+       <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+       <div class="col-sm-10">
+         <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Password" required>
+         <?php echo $errPass; ?>
+       </div>
+     </div>
+     <div class="form-group row">
+       <div class="offset-sm-2 col-sm-10">
+         <input type="submit" value="Register" name="register" class="btn btn-primary"/>
+       </div>
+     </div>
+   </form>
 
-    <h4> Already a User? Login here </h4>
-    <form method="post">
-        <input type="submit" name="buttonSignIn"
-                class="button" value="Sign In" />
-    </form>
-    <?php
-     if(array_key_exists('buttonSignIn', $_POST)) {
-         header("location: login.php");
-     }
-     ?>
-
- </body>
- </html>
-
-<?php
-function pre_r($array) {
-    echo '<pre>';
-    print_r($array);
-    echo '</pre>';
-}
-?>
+   <h4> Already a User? Login here </h4>
+<form method="post">
+    <input type="submit" name="buttonSignIn"
+            class="button" value="Sign In" />
+</form> -->
+ </div>
+ <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
--- register.php
+++ register.php
@@ -10,10 +10,16 @@
    session_start();
    //if(!empty($_SESSION['loggedin'])){ header("location: home.php");}  //check if user already logged in
 
+
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $Firstname = $_POST['Firstname'];
-    $Surname = $_POST['Surname'];
+    $firstname = $_POST['firstname'];
+    $surname = $_POST['surname'];
+
+    echo $email;
+    echo $password;
+    echo $firstname;
+    echo $surname;
 
     require_once 'includes/dbh.inc.php';
     $conn = createConnection("sql100.epizy.com", "epiz_31242413", "WbIh2OaPZju", "epiz_31242413_project_database");
@@ -21,9 +27,25 @@
     if($conn->connect_error){
         die('Connection failed : '.$conn->connect_error);
     } else {
+        if (empty($email)) {
+          // code...
+          echo "email is empty";
+        }
+        if (empty($password)) {
+          // code...
+          echo "password is empty";
+        }
+        if (empty($firstname)) {
+          // code...
+          echo "firstname is empty";
+        }
+        if (empty($surname)) {
+          // code...
+          echo "surname is empty";
+        }
         $stmt = $conn->prepare("insert into user(handle, Password, Firstname, Surname)
         values(?,?,?,?)");
-        $stmt->bind_param("ssss",$email, $password, $Firstname, $Surname);
+        $stmt->bind_param("ssss",$email, $password, $firstname, $surname);
         $stmt->execute();
         echo "User registered, redirecting to login";
         $stmt->close();
