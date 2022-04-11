6c1b563369a73cc2e5d10ba0fa554553f86f666a
seanfitz121 <63353392+seanfitz121@users.noreply.github.com>
2022-04-11 23:21:45 +0100

    Update auth.php

--- auth.php
+++ auth.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>Auth</title>
+    <link rel="icon" href="images/uniConnectLogo.png" />
     <!-- <meta http-equiv = "refresh" content = "3; url = http://14-cs4116.infinityfreeapp.com/profilePage.php" /> -->
 </head>
 <body>
