6dc48f20f09d11a74f8f2e18d5167aaee8fc37ae
seanfitz121 <63353392+seanfitz121@users.noreply.github.com>
2022-04-11 23:16:21 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
  <html>
  <head>
+  <link rel="icon" href="images/uniConnectLogo.png" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
    <title>Login</title>
  </head>
  <body>
@@ -9,16 +11,27 @@
         if(!empty($_SESSION['loggedin'])){ header("location: home.php");}  //check if user already logged in
         //echo "Test login page";
     ?>
-
+    <div class="container">
+    <div class="row" style="min-width:800">
+      <div class="col-sm-1"><img src="images/uniConnectLogo.png" alt="Logo" ></div>
+      
+     </div>
     <h2>Login</h2>
     <form action="auth.php" method="POST">
     <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" id="email" class="form-control" name="email" required/>
+        <div class="col-sm-3 control-label">
+            <label for="email" class="col-sm-2 col-form-label">Email</label> 
+            
+        <!--    </div>
+        <div class="col-sm-10"> -->
+        <input type="email" id="email" class="form-control" name="email"/> 
+        </div>
     </div>
     <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" id="password" class="form-control" name="password" required/>
+    <div class="col-sm-3 control-label">
+            <label for="password" class="col-sm-2 col-form-label">Password</label>
+            
+            <input type="password" id="password" class="form-control" name="password"/>
     </div>
         <input type="submit" class="btn btn-primary" value="Login" name="">
     </form>
@@ -34,6 +47,5 @@
          <input type="submit" name="buttonRegister"
                  class="button" value="Register" />
      </form>
-
  </body>
  </html>
