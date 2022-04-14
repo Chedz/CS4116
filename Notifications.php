d9cefdf087939e83e2cc21db79fd55cfa6ba7a53
David <dmdcondon@gmail.com>
2022-04-14 23:15:36 +0100

    added Notifications php file

---
+++ Notifications.php
@@ -0,0 +1,7 @@
+<?php     
+   require_once 'includes/dbh.inc.php';
+   $mysqli = createConnection("sql100.epizy.com", "epiz_31242413", "WbIh2OaPZju", "epiz_31242413_project_database");
+
+   $sql = "INSERT INTO connections"
+
+?>
