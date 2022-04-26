86856c4f53d160aaa2c9d6e0dce90cfc5cb44794
Eoin Chedzey <eoinisnow@gmail.com>
2022-04-26 17:22:39 +0100

    fixed connections functionality, now displaying user matches

--- connections.php
+++ connections.php
@@ -36,28 +36,28 @@
    <div class="container-fluid">
    <a class= "navbar-brand" href="index.php"><img src="images/uniConnectLogo.png"></a>
    <button class="navbar-toggler" type="button" data-toggle="collapse"
-   data-target="#navbarResponsive"> 
+   data-target="#navbarResponsive">
         <span class="navbar=toggler-icon"></span>
-    </button>    
+    </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item">   
+            <li class="nav-item">
                 <a class="nav-link" href="index.php">Home</a>
             </li>
 
-            <li class="nav-item">   
+            <li class="nav-item">
                 <a class="nav-link" href="profilePage.php"><span class="glyphicon glyphicon-user"></span> Profile</a>
             </li>
 
-            <li class="nav-item">   
+            <li class="nav-item">
                 <a class="nav-link" href="search.php"><span class="glyphicon glyphicon-user"></span> Search</a>
             </li>
 
-            <li class="nav-item">   
+            <li class="nav-item">
                 <a class="nav-link" href="connections.php"><span class="glyphicon glyphicon-user"></span> Matches</a>
             </li>
 
-            <li class="nav-item">   
+            <li class="nav-item">
                 <a class="nav-link" href="#">About Us</a>
             </li>
 
@@ -72,60 +72,104 @@
 <!--Connections Feed Here-->
     <div class="container-fluid">
       <p>Connections here</p>
-    </div>
+
     <?php
      require_once 'includes/dbh.inc.php';
-     $conn = createConnection("sql100.epizy.com", "epiz_31242413", "WbIh2OaPZju", "epiz_31242413_project_database");
+     $matchConn = createConnection("sql100.epizy.com", "epiz_31242413", "WbIh2OaPZju", "epiz_31242413_project_database");
      require_once 'profilePreview.php';
      //Function to get the social media accounts (Instagram and Snapchat) of a user given an userID
-     function getsocials($userID){ 
+     function getsocials($userID){
       $sqlSocial = "SELECT * FROM profile WHERE UserID = '$userID'";
-      $socialResults = mysqli_query($conn, $sqlSocial); 
+      $socialResults = mysqli_query($matchConn, $sqlSocial);
       $socialRow = mysqli_fetch_array($socialResults);
       //Prints out Instagram and Snapchat of user put into function
-      echo "<br>" . $socialRow['Instagram'] . "<br>" . $socialRow['Snapchat']; 
+      echo "<br>" . $socialRow['Instagram'] . "<br>" . $socialRow['Snapchat'];
      }
 
      $tempUserID = $_SESSION['username'];
                 $sql1 = "SELECT UserID FROM user WHERE Handle = '$tempUserID'";
-                $results1 = mysqli_query($conn,$sql1);
+                $results1 = mysqli_query($matchConn,$sql1);
                 $row1 = mysqli_fetch_array($results1);
                 $currUserID = $row1['UserID']; //initiated connection
-                //echo $User1ID;
 
-     //Get all the Connections made by the user that have been accepted 
-     $sqlconnections = "SELECT * FROM Connections WHERE (user1ID  = '$currUserID' OR user2ID = '$currUserID) AND isAccepted = 1 ";
-     $results = mysqli_query($conn, $sqlconnections);
-     if (mysqli_num_rows($results) == 0){
-       // The query returned 0 rows!
-       echo 'no Matches'; 
-     } else {
-      $isAcceptedArray = array(); 
-      foreach($results as $temprow){
-      //$isAcceptedArray[] = $temprow['user2ID'];
-        if ($currUserID = $temprow['user1ID']){
-          $isAcceptedArray[] = $temprow['user1ID']; 
-        } else {
-          $isAcceptedArray[] = $temprow['user2ID']; 
-        }
-      }
-      //Present profile preview of the users that have made connections with the current user
-      foreach($isAcceptedArray as $id) {
-        $otherUserID = $id; 
-        $sql2 = "SELECT * FROM profile WHERE UserID = '$otherUserID'";
-        $results2 = mysqli_query($conn,$sql2);
-        $row2 = mysqli_fetch_array($results2);
-        $tempMail = $row2['email'];
-        getProfilePreview($tempMail);
-        printf("<br>");	
-        getsocials($otherUserID); 
-        printf("<br>"); 		    
-      }
+
+
+     //Get all the Connections made by the user that have been accepted
+     $sqlconnections = "SELECT * FROM Connections WHERE userID1  = '$currUserID' OR userID2 = '$currUserID' AND isAccepted = 1";
+     $matchResults = mysqli_query($matchConn, $sqlconnections);
+     // if (mysqli_num_rows($results) == 0){
+     //   // The query returned 0 rows!
+     //   echo 'no Matches';
+     // } else {
+     //    echo "Found a match(s)";
+     //    printf($results);
+
+     $totalMatchIds = array();
+
+     if ($matchResults) {
+       if(mysqli_num_rows($matchResults)>0){
+         // echo "Matches found";
+         // echo count($matchResults);
+         while($row = mysqli_fetch_array($matchResults)){ //loop through each result and store as row var
+             //print_r($row);
+             // $user1 = $row['userID1'];
+             // $user2 = $row['userID2'];
+             // echo $user1;
+             // echo " , ";
+             // echo $user2;
+
+             if ($currUserID = $row['userID1']){ //select id of user that is not curUser
+               $matchUserId = $row['userID1'];
+             } else {
+               $matchUserId = $row['userID2'];
+             }
+             array_push($totalMatchIds, $matchUserId); //push userId of other user within match to array
+             // echo "pushed row ";
+             // echo $matchUserId;
+           }
+
+           //print_r($totalMatchIds);
+
+
+           foreach($totalMatchIds as $id) {
+             $otherUserID = $id;
+             $sql2 = "SELECT * FROM profile WHERE UserID = '$otherUserID'";
+             $results2 = mysqli_query($matchConn,$sql2);
+
+             $row2 = mysqli_fetch_array($results2);
+             $tempMail = $row2['email'];
+
+             //echo $tempMail;
+             getProfilePreview($tempMail);
+             printf("<br>");
+             getsocials($otherUserID);
+             printf("<br>");
+           }
+
+
+       }else {
+         echo "no Matches";
+       }
      }
-     
-    
-    ?>
 
+      // echo implode( ", ", $results );
+      //Present profile preview of the users that have made connections with the current user
+      // foreach($isAcceptedArray as $id) {
+      //   $otherUserID = $id;
+      //   $sql2 = "SELECT * FROM profile WHERE UserID = '$otherUserID'";
+      //   $results2 = mysqli_query($conn,$sql2);
+      //   $row2 = mysqli_fetch_array($results2);
+      //   $tempMail = $row2['email'];
+      //   getProfilePreview($tempMail);
+      //   printf("<br>");
+      //   getsocials($otherUserID);
+      //   printf("<br>");
+      // }
+     //}
+     $matchConn->close();
+
+    ?>
+</div>
 
     <!-- <form action="profilePage.php">
         <input type="submit" value="Profile" />
