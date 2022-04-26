64e4876f580fdfb2174e75912f39c05cc259a7cf
Danielz110 <64483972+Danielz110@users.noreply.github.com>
2022-04-26 19:05:26 +0100

    Update editProfile.php
    Nav bar added and styling

--- editProfile.php
+++ editProfile.php
@@ -1,44 +1,60 @@
 <!DOCTYPE html>
  <html>
  <head>
-  <meta http-equiv = "refresh" content = "3; url = http://14-cs4116.infinityfreeapp.com/profilePage.php" />
+    <title>Profile change</title>
+    <meta http-equiv = "refresh" content = "3; url = http://14-cs4116.infinityfreeapp.com/profilePage.php" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="images/uniConnectLogo.png" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   <title>Profile change</title>
  </head>
+ <style>
+    body {
+    background-color: #868c93;
+    }
+
+ </style>
  <body>
-   
-   <!-- Navbar-->
-     <nav class="navbar navbar-dark bg-dark">
-       <div class="container">
-       <!-- <div class="navbar-header">
-         <a class="navbar-brand" href="#">WebSiteName</a>
-       </div> -->
-       <a href="home.php" class="navbar-left"><img src="images/uniConnectLogo.png"></a>
-       <ul class="nav navbar-nav">
-         <li class="active"><a href="home.php">Home</a></li>
-       </ul>
-       <ul class="nav navbar-nav">
-         <li><a href="connections.php">Matches</a></li>
-       </ul>
-       <ul class="nav navbar-nav">
-         <li><a href="search.php">Search</a></li>
-       </ul>
-       <ul class="nav navbar-nav navbar-right">
-         <li><a href="profilePage.php"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
-         <!-- <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
-         <form method="post">
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+   <div class="container-fluid">
+   <a class= "navbar-brand" href="index.php"><img src="images/uniConnectLogo.png"></a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse"
+   data-target="#navbarResponsive"> 
+        <span class="navbar=toggler-icon"></span>
+    </button>    
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">   
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+
+            <li class="nav-item">   
+                <a class="nav-link" href="profilePage.php"><span class="glyphicon glyphicon-user"></span> Profile</a>
+            </li>
+
+            <li class="nav-item">   
+                <a class="nav-link" href="search.php"><span class="glyphicon glyphicon-user"></span> Search</a>
+            </li>
+
+            <li class="nav-item">   
+                <a class="nav-link" href="connections.php"><span class="glyphicon glyphicon-user"></span> Matches</a>
+            </li>
+
+            <li class="nav-item">   
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+
+            <form method="post">
              <input type="submit" name="buttonLogOut"
-                     class="btn btn-primary" value="Log out" />
+                     class="btn btn-primary" value="Logout" />
          </form>
-       </ul>
-     </div>
-     </nav>
+ </div>
+ </nav>
 
     <br>
+
    <?php
     require_once 'includes/dbh.inc.php';
     $conn = createConnection("sql100.epizy.com", "epiz_31242413", "WbIh2OaPZju", "epiz_31242413_project_database");
@@ -76,25 +92,30 @@
     $InterestID = $_POST['interests'];
     $InterestID2 = $_POST['interests2'];
     $InterestID3 = $_POST['interests3'];
-    
+
     //Upload file + path
     $targetDirectory = "userImages/";
-    $targetFile = $targetDirectory . basename($_FILES["userImage"]["name"]);
+    if(isset($_FILES['userImage'])){
+            $targetFile = $targetDirectory . basename($_FILES["userImage"]["name"]);
+    } else {
+        $targetFile = $targetDirectory . "defaultProfilePic.png";
+    }
+    
     $uploadSuccess = 1;
     $fileTypesAllowed = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
 
     
     if($fileTypesAllowed != "jpg" && $fileTypesAllowed != "png" && $fileTypesAllowed != "jpeg"){
-        echo "File type not allowed";
+        echo "<p><center>File type not allowed</center></p>";
         $uploadSuccess = 0;
     }
     if($uploadSuccess == 0){
-        echo "File was not uploaded";
+        echo "<p><center>File was not uploaded</center></p>";
     } else {
         if(move_uploaded_file($_FILES["userImage"]["tmp_name"], $targetFile)){
-            echo "File " . htmlspecialchars(basename($_FILES["userImage"]["name"])) . " has been uploaded.";
-        } else {
-            echo "There was an issue with uploading the file";
+            
+        } else {   
+            echo "<center><p>There was an issue with uploading the file</center></p>";
         }
     }
 
@@ -106,18 +127,24 @@
         $results = mysqli_query($conn, $sql);
         if($results){
             if(mysqli_num_rows($results)>0){ //IF UserID exists in profile table, update data in corresponding row
-                $stmt = $conn->prepare("UPDATE profile SET Firstname = '$firstName', Surname = '$surname', Age='$age', Smoker='$smoker', Drinker='$drinker', Gender='$gender', Seeking='$seeking', Institution='$university', Course='$course', Location='$location', Instagram='$instagram',                   Snapchat='$snapchat', Occupation='$occupation', Description='$description', email='$userEmail', Photo='$targetFile' WHERE UserID='$UserID'");
+                if(empty($_FILES['userImage'])){
+                    $stmt = $conn->prepare("UPDATE profile SET Firstname = '$firstName', Surname = '$surname', Age='$age', Smoker='$smoker', Drinker='$drinker', Gender='$gender', Seeking='$seeking', Institution='$university',                                               Course='$course', Location='$location', Instagram='$instagram', Snapchat='$snapchat', Occupation='$occupation', Description='$description', email='$userEmail' WHERE UserID='$UserID'");
+                } else {
+                    $stmt = $conn->prepare("UPDATE profile SET Firstname = '$firstName', Surname = '$surname', Age='$age', Smoker='$smoker', Drinker='$drinker', Gender='$gender', Seeking='$seeking', Institution='$university',                                               Course='$course', Location='$location', Instagram='$instagram', Snapchat='$snapchat', Occupation='$occupation', Description='$description', email='$userEmail', Photo='$targetFile' WHERE UserID='$UserID'");
+                }
                 $stmt->execute();
-                echo "Profile Updated";
-                
+                echo "<center><p>Profile Updated</center></p>";
                 $stmt->close();
-            } else { //ELSE username doesnt exist, insert new row
+
+                
+            } else { //ELSE UserID doesnt exist, insert new row
                 $stmt = $conn->prepare("INSERT INTO profile (UserID, Firstname, Surname, Age, Smoker, Drinker, Gender, Seeking, Institution, Course, Location, Instagram, Snapchat, Occupation, Description, email, Photo)
                     values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
                 $stmt->bind_param('issiisssssssssbss',$UserID, $firstName, $surname, $age, $smoker, $drinker, $gender, $seeking, $university, $course, $location, $instagram, $snapchat, $occupation, $description, $userEmail, $targetFile);
                 $stmt->execute();
-                echo "Profile Updated";
+                echo "<center><p>Profile Updated!</center></p>";
                 $stmt->close();
+                
             }
         }
     }
@@ -130,22 +157,20 @@
             if(mysqli_num_rows($results)>0){ //IF UserID exists in Interests table, update data in corresponding row
                 $stmt = $conn->prepare("UPDATE Interests SET InterestID='$InterestID', InterestID2 = '$InterestID2', InterestID3 = '$InterestID3' WHERE UserID='$UserID'");
                 $stmt->execute();
-                echo "Interests Updated";
-                
                 $stmt->close();
             } else { //ELSE UserID doesnt exist, insert new row
                 $stmt = $conn->prepare("INSERT INTO Interests (UserID, InterestID, InterestID2, InterestID3)
                     values(?,?,?,?)");
                 $stmt->bind_param('iiii',$UserID, $InterestID, $InterestID2, $InterestID3);
                 $stmt->execute();
-                echo "Interests Updated";
                 $stmt->close();
             }
         }
         $conn->close();
     }
 ?>
- <!--Footer-->
+
+    <!--Footer-->
     <br>
      <footer class="bg-light text-center text-lg-start">
     <!-- Copyright -->
@@ -155,5 +180,6 @@
     </div>
     <!-- Copyright -->
   </footer>
+
  </body>
  </html>
