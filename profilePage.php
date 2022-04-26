dd337c4ba0ca71c200fd7998065e59a0b26207a9
seanfitz121 <63353392+seanfitz121@users.noreply.github.com>
2022-04-26 23:57:00 +0100

    Update profilePage.php

--- profilePage.php
+++ profilePage.php
@@ -1,3 +1,20 @@
+<?php
+    require_once 'includes/dbh.inc.php';
+    $tempUserID = $_SESSION['username'];
+    $conn = createConnection("sql100.epizy.com", "epiz_31242413", "WbIh2OaPZju", "epiz_31242413_project_database");
+    $sqlBanned = "SELECT * FROM profile WHERE email = '$tempUserID'";
+    $sqlBannedRes = mysqli_query($conn,$sqlBanned);
+    $rowBan = mysqli_fetch_array($sqlBannedRes);
+    $isBanned = $rowBan['Banned'];
+    if($isBanned == NULL || $isBanned == 0){
+        //Do nothing, user is not banned
+    } else if($isBanned == 1){
+        //User is banned, redirect
+        header("location: userBanned.php");
+    }
+    $conn->close();
+    
+ ?>
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
